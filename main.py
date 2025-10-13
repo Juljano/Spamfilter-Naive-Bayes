650a4a6fc72f3a58f073877d74a10c3ccc85e65b
Janosch <Janosch@anoym.de>
2025-10-13 14:09:20 +0200

    Auslesen der Spam-Nachrichten aus der CSV-Datei Erste Schritte für das Oversample der Trainingsdaten

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from imblearn.over_sampling import RandomOverSampler
+
+def read_trainings_data(path):
+    try:
+        read_csv =  pd.read_csv(path,  encoding='latin1') # Downloaded from Kaggle.com
+        x = read_csv["v1"].tolist()
+        y = read_csv["v2"].tolist()
+        return x,y
+    except FileNotFoundError:
+        print("File not found - Please check the folder name")
+        return None,None
+
+def oversample_trainings_data(x,y):
+    x_array = np.array(x).reshape(-1, 1) #Convert 1D-Array to 2D-Array
+    X_res, y_res = RandomOverSampler(random_state=42).fit_resample(x_array, y)
+    print("Resampled Labels:", y_res)
+
+    return X_res, y_res
+
+
+
+if __name__ == "__main__":
+    x, y = read_trainings_data('Training/spam.csv')
+    oversample_trainings_data(x,y)
